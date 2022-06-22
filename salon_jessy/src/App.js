477e815546f984d959a6244f98b2d9359959cecb
mairenaRo <ronny.mairena.r@hotmail.com>
2022-06-21 18:49:14 -0600

    Cambios del react

--- salon_jessy/src/App.js
+++ salon_jessy/src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import NavBarElements from './components/NavBarElements';
+import {BrowserRouter as Router} from 'react-router-dom';
 
 function App() {
   return (
+    <Router>
       <NavBarElements />
+    </Router>
+      
   )
 }
 
