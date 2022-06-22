5bfda95d9a080d85807ea6ae2c7642069ee7de93
mairenaRo <ronny.mairena.r@hotmail.com>
2022-06-21 23:15:21 -0600

    Navbar terminado

--- salon_jessy/src/Components/NavBarElements.js
+++ salon_jessy/src/Components/NavBarElements.js
@@ -4,9 +4,10 @@ const NavBarElements = () => {
     return (
         <nav className="navbar navbar-expand-lg">
         <div className="container-fluid">
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="true" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
+        <h1>LOGO</h1>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button> 	
           <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav">
               <li className="nav-item">
