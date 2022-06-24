bf27aa3d82cd6f6036b6d2851744e1cd4538e4ba
mairenaRo <ronny.mairena.r@hotmail.com>
2022-06-24 11:28:44 -0600

    Update Login.js

--- salon_jessy/src/Components/Login.js
+++ salon_jessy/src/Components/Login.js
@@ -1,31 +1,4 @@
-<<<<<<< Updated upstream
-import React from 'react'
 
-function Login() {
-  return (
-   <form>
-        <div className="form-inner">
-            <h2> LOGIN</h2>
-            <div className=" form-group">
-                <label htmlFor="Nombre">Name: </label>
-                <input type="text" name="Nombre" id="Nombre" />
-            </div>
-
-            <div className="form-group">
-                <label htmlFor="Correo">Email:</label>
-                <input type="Correo" name="Correo" id ="Correo" />
-            </div>
-
-            <div className="form-group">
-                <label htmlFor="Contraseña">Contraseña:</label>
-                <input type="Contraseña" name="Contraseña" id="Contraseña" />
-            </div>    
-
-            <input type="submit" value="LOGIN" />
-        </div>
-   </form>
-  )
-=======
 import React from 'react';
 
 const Login = () => {
@@ -50,7 +23,6 @@ const Login = () => {
             </form>
         </div>
     );
->>>>>>> Stashed changes
 }
 
 export default Login
